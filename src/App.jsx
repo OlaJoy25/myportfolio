d9bbc13fe9cdab39d0d0fcf79df38e80a5df70f5
OlaJoy25 <90443586+OlaJoy25@users.noreply.github.com>
2023-07-24 14:42:12 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Joycelyn Ajike Ola",
+  title: "Cybersecurity Analyst",
+  email: "olajoycelyn.ak@gmail.com",
+  gitHub: "https://github.com/OlaJoy25",
+  instagram: "",
+  linkedIn: "Joycelyn Ajike Ola",
+  medium: "https://medium.com/@Cybertea_with_Jay",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
