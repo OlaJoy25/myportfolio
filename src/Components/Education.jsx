c1184f1cb154b16d4e39a5d55c45fe39f54886a5
OlaJoy25 <90443586+OlaJoy25@users.noreply.github.com>
2023-07-24 16:31:37 +0000

    done

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,42 +1,12 @@
 import React from "react";
 
-import image from "../images/motion-background.jpg";
-
-const imageAltText = "purple and blue abstract background";
-
 const Education = () => {
   return (
     <section className="light" id="education">
       <h2>Education</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
-            src={image}
-            style={{
-              height: "90%",
-              width: "100%",
-              objectFit: "cover",
-              animation: "1s ease-out 0s 1 slideIn",
-            }}
-            alt={imageAltText}
-          />
-        </div>
-      </div>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}></div>
     </section>
   );
 };
-const schoolList = [
-  {
-    name: "African Leadership University, Kigali, Rwanda",
-    description: "Final year of BSC. Computer Science",
-  },
-];
-<div className="container">
-  {schoolList.map((school) => (
-    <div className="box" key={school.name}>
-      <p className="small">{school.description}</p>
-    </div>
-  ))}
-</div>;
 
 export default Education;
