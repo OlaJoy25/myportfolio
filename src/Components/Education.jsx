b1f6770d986e665ce549e040e5e5031cd6b86883
OlaJoy25 <90443586+OlaJoy25@users.noreply.github.com>
2023-07-24 16:29:26 +0000

    final

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
 const Education = () => {
   return (
     <section className="light" id="education">
       <h2>Education</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}></div>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{
+              height: "90%",
+              width: "100%",
+              objectFit: "cover",
+              animation: "1s ease-out 0s 1 slideIn",
+            }}
+            alt={imageAltText}
+          />
+        </div>
+      </div>
     </section>
   );
 };
