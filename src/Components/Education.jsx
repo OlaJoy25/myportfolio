2b07cf3c26deeb0f089ef1de336cba869020e151
OlaJoy25 <90443586+OlaJoy25@users.noreply.github.com>
2023-07-24 16:24:37 +0000

    work

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,8 +4,22 @@ const Education = () => {
   return (
     <section className="light" id="education">
       <h2>Education</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}></div>
     </section>
   );
 };
+const schoolList = [
+  {
+    name: "African Leadership University, Kigali, Rwanda",
+    description: "Final year of BSC. Computer Science",
+  },
+];
+<div className="container">
+  {schoolList.map((school) => (
+    <div className="box" key={school.name}>
+      <p className="small">{school.description}</p>
+    </div>
+  ))}
+</div>;
 
 export default Education;
